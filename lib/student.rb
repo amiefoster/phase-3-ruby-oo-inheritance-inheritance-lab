e2168388db8cfad3510a586431667f4d486a34ca
Amie Foster <amie.n.foster@gmail.com>
2021-10-14 17:07:50 -0600

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'user'
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        super
+        @knowledge = []
+    end
+
+    def learn string_of_knowledge
+        @knowledge << string_of_knowledge
+
+    end
 
 end
